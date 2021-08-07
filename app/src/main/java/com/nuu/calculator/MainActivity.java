f5aeb6f1e22a39b176e37e1b67261abf2fc0b9b9
s5846283 <s5846283@mds.com.tw>
2021-08-07 12:26:22 +0800

    建立登入畫面

--- app/src/main/java/com/nuu/calculator/MainActivity.java
+++ app/src/main/java/com/nuu/calculator/MainActivity.java
@@ -1,14 +1,45 @@
 package com.nuu.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.nuu.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        context = this;
+
+        initListener();
+    }
+
+    private void initListener(){
+        binding.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.edtName.getText().toString();
+                if(checkName(name)){
+                    Intent intent  = new Intent();
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(context, getString(R.string.name_hint), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private Boolean checkName(String name){
+        return name != null && !name.isEmpty();
     }
 }
